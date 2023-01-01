f100268efeec3b9902d13c3c2457d30c9d6a7d95
jck0day <jck0day@gmail.com>
2023-01-01 03:12:55 +0100

    On repart de zero avec la bonne config

---
+++ src/main/java/xhookman/cursedmod/Cursedmod.java
@@ -0,0 +1,10 @@
+package xhookman.cursedmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Cursedmod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/xhookman/cursedmod/client/CursedmodClient.java
@@ -0,0 +1,70 @@
+package xhookman.cursedmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.sound.PositionedSoundInstance;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import org.lwjgl.glfw.GLFW;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.UUID;
+
+@Environment(EnvType.CLIENT)
+public class CursedmodClient implements ClientModInitializer {
+    public static final String MOD_ID = "cursedmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final Identifier MY_SOUND_ID = new Identifier(MOD_ID+":mouche");
+    public static SoundEvent MY_SOUND_EVENT = new SoundEvent(MY_SOUND_ID);
+    @Override
+    public void onInitializeClient() {
+        LOGGER.info("ATTENTION CA VA CRASH !!! (" + MOD_ID + " est chargé)");
+        Registry.register(Registry.SOUND_EVENT, CursedmodClient.MY_SOUND_ID, MY_SOUND_EVENT);
+        KeyBinding keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.cursedmod.spook", // The translation key of the keybinding's name
+                InputUtil.Type.KEYSYM, // The type of the keybinding, KEYSYM for keyboard, MOUSE for mouse.
+                GLFW.GLFW_KEY_R, // The keycode of the key
+                "category.cursedmod.test" // The translation key of the keybinding's category.
+        ));
+
+        PositionedSoundInstance sound = PositionedSoundInstance.master(MY_SOUND_EVENT, 1.0F);
+
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            PlayerEntity player = client.getInstance().player;
+
+            //get the server if the world is client side or server side
+
+			/*try {
+				server = player.getServer();
+			} catch (Exception e) {
+				LOGGER.info("Le serveur n'est pas lancé");
+			}*/
+
+            while (keyBinding.wasPressed()) {
+
+                UUID testuuid = client.player.getUuid();
+                client.player.sendMessage(Text.of(testuuid.toString()), false);
+
+
+                //player.sendMessage(Text.of("C'est la big mouche qui te pique !"), true);
+
+				if(!client.getSoundManager().isPlaying(sound)){
+					client.getSoundManager().play(sound);
+				}
+                //send a public message in server chat
+                //client.getServer().getWorld(ServerWorld.OVERWORLD).playSound(null, player.getX(), player.getY(), player.getZ(), MY_SOUND_EVENT, SoundCategory.PLAYERS, 1.0F, 1.0F);
+            }
+        });
+    }
+}
